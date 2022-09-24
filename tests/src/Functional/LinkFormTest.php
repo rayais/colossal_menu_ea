b865b2cbea4a7c2470ac8c6a8f02ed928cedf7ac
alexanderj <alexanderj@3722725.no-reply.drupal.org>
2022-09-24 02:34:33 +0000

    Issue #3310937 by alexanderj, mpaulo: Fix test \Drupal\Tests\colossal_menu\Functional\LinkFormTest::testEditLink

--- tests/src/Functional/LinkFormTest.php
+++ tests/src/Functional/LinkFormTest.php
@@ -44,22 +44,32 @@ class LinkFormTest extends ColossalMenuFunctionalTestBase {
    * Tests editing a Link.
    */
   public function testEditLink() {
+    // Create link.
     $link_type = \Drupal::entityTypeManager()->getStorage('colossal_menu_link_type')->load('test_type');
-    $link = \Drupal::entityTypeManager()->getStorage('colossal_menu_link')->create([
-      'type' => $link_type->id(),
-      'title' => 'Front page',
-      'link' => '/node',
-    ]);
-    $link->save();
+    $this->drupalGet(Url::fromRoute('entity.colossal_menu_link.add_form', [
+      'colossal_menu' => 'tests',
+      'colossal_menu_link_type' => $link_type->id(),
+    ]));
+    $this->submitForm([
+      'title[0][value]' => 'Front page',
+      'link[0][uri]' => '/node',
+    ], 'Save');
+    $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
+
+    // Edit link.
+    $link = \Drupal::entityTypeManager()->getStorage('colossal_menu_link')->load(1);
     $this->drupalGet($link->toUrl('edit-form'));
     $this->assertSession()->pageTextContains("Edit {$link->label()}");
     $this->assertSession()->linkByHrefExists($link->toUrl('delete-form')->toString());
-    $this->submitForm(['label' => 'Updated front page', 'link' => '<front>'], 'Save');
+    $this->submitForm([
+      'title[0][value]' => 'Updated front page',
+      'link[0][uri]' => '<front>',
+    ], 'Save');
     $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
     $this->assertSession()->pageTextContains("Link Updated front page updated.");
     \Drupal::entityTypeManager()->getStorage('colossal_menu_link')->resetCache();
-    $link = \Drupal::entityTypeManager()->getStorage('colossal_menu_link_type')->load($link->id());
-    $this->assertEquals('<front>', $link->link->uri);
+    $link = \Drupal::entityTypeManager()->getStorage('colossal_menu_link')->load($link->id());
+    $this->assertEquals('internal:/', $link->link->uri);
   }
 
 }
